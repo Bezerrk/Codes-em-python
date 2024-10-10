8f210b40dcd001cc4d6bdbff0da48700369b9398
Bezerrk <Caua0007@gmail.com>
2024-10-10 12:27:19 -0400

    Create sort.py

---
+++ sort.py
@@ -0,0 +1,7 @@
+lista = [1,2,3,5,4,3,2,1,6,7,5,8,6,4,0]
+#faz um sort simples e imprime os itens em ordem crescente
+lista.sort()
+print (lista)
+#inverte a ordem dos itens e imprime
+lista.reverse()
+print (lista)
